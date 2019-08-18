1074df06b5dd19b0f0d2417368faac58c32a04e9
Kayo Bruno <kayodw@gmail.com>
2019-08-17 21:03:24 -0300

    Configurar projeto
    Configurações básicas do projeto

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models\User;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -25,15 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password',
     ];
 }
