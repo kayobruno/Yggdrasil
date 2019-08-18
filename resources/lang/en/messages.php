8181c59e0e23486065ba8ab21c8d0e75cdb0d199
Kayo Bruno <kayodw@gmail.com>
2019-08-17 21:37:09 -0300

    Configurar traduções
    Configurar traduções pt-br e en

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'error' => [
+        'forbidden' => 'Access denied',
+        'unavailable' => 'Oops, it looks like something went wrong',
+        'unauthorized' => 'Unauthorized',
+        'notfound' => 'Not found',
+    ],
+
+    'success' => [
+        'created' => 'Successfully created',
+        'updated' => 'Successfully updated',
+        'removed' => 'Successfully removed',
+    ],
+];
---
+++ resources/lang/pt-br/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'error' => [
+        'forbidden' => 'Acesso negado',
+        'unavailable' => 'Oops, parece que algo deu errado',
+        'unauthorized' => 'Não autorizado',
+        'notfound' => 'Não encontrado',
+    ],
+
+    'success' => [
+        'created' => 'Cadastrado com sucesso',
+        'updated' => 'Atualizado com sucesso',
+        'removed' => 'Removido com sucesso',
+    ],
+];
