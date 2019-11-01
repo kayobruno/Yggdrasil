f6eb0a1641774cfedc41b32f85505e115cfacf9c
Kayo Queiroz <kayo.queiroz@gsd.com.br>
2019-11-01 20:35:49 -0300

    Adicionar documentação do IDE Helper nos Models
    Foram adicionados comentários nos Models para o funcionamento correto
    do IDE Helper.

--- app/Models/Permission/Permission.php
+++ app/Models/Permission/Permission.php
@@ -4,6 +4,28 @@ namespace App\Models\Permission;
 
 use Zizaco\Entrust\EntrustPermission;
 
+/**
+ * App\Models\Permission\Permission
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Role\Role[] $roles
+ * @property-read int|null $roles_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Permission\Permission whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Permission extends EntrustPermission
 {
 }
--- app/Models/Role/Role.php
+++ app/Models/Role/Role.php
@@ -4,6 +4,30 @@ namespace App\Models\Role;
 
 use Zizaco\Entrust\EntrustRole;
 
+/**
+ * App\Models\Role\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Permission\Permission[] $perms
+ * @property-read int|null $perms_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Role\Role whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Role extends EntrustRole
 {
 }
